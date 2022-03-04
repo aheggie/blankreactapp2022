ad7dcf10ed8552388e9a0bca9f6da9f47aa55fe2
Alexandra Heggie <26582461+aheggie@users.noreply.github.com>
2022-03-03 23:15:13 -0500

    prettierjust changed the single to double quotes i think

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import {StrictMode} from 'react';
-import {render} from 'react-dom';
-import './style.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import "./style.css";
+import App from "./App.tsx";
+import reportWebVitals from "./reportWebVitals";
 
 render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // log web vitals
